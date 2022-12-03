297637cda3d9d0c5fd42a20b6086a7f71fa34256
JulSeb42 <julien.sebag@me.com>
2022-12-03 01:12:42 +0100

    Test deploy

--- client/src/graphql/apollo-client.ts
+++ client/src/graphql/apollo-client.ts
@@ -3,7 +3,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 import { setContext } from "@apollo/client/link/context"
 
-const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI
+// const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI
+const GRAPHQL_URI = "https://blog-apollo.vercel.app/graphql"
 
 const authLink = setContext((_, { headers }) => {
     return {
